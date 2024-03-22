6055ec178518021d85c6438900adec16d4a0fdba
BrentGruber <brent.gruber77@gmail.com>
2024-03-22 01:30:34 -0400

    fixing extracting of outputs

--- scripts/create_admin_user.sh
+++ scripts/create_admin_user.sh
@@ -4,8 +4,8 @@ echo "checking for admin user"
 output=$(flyctl ssh console --command "su - git -c '/usr/local/bin/gitea admin user list'" | grep "gitea")
 if [ -z "$output" ]; then
   user_output=$(flyctl ssh console --command "su - git -c '/usr/local/bin/gitea admin user create --username=gitea --random-password --email=gitea@brentgruber.com --admin --must-change-password=false --access-token'") 
-  password=$(echo $user_output | sed -n "/generated random password is/ s/.*'\(.*\)'.*/\1/p")
-  access_token=$(echo $user_output | sed -n "/Access token was successfully created/ s/.* \(.*\)$/\1/p")
+  password=$(echo $user_output | sed -n "s/generated random password is '\([^']*\)'.*/\1/p")
+  access_token=$(echo $user_output | sed -n -e 's/^.*Access token was successfully created... \([^ ]*\).*$/\1/p')
   doppler secrets set GITEA_ADMIN_USER=gitea >/dev/null 2>&1 # ignore output
   doppler secrets set GITEA_ADMIN_PASSWORD=$password >/dev/null 2>&1 #ignore output
   doppler secrets set GITEA_ADMIN_ACCESS_TOKEN=$access_token >/dev/null 2>&1 #ignore output
