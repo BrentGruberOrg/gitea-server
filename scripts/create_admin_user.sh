0dd2e5f33c929a3556dd6a346aff348ebe1e727e
BrentGruber <brent.gruber77@gmail.com>
2024-03-22 01:00:56 -0400

    fixing script

--- scripts/create_admin_user.sh
+++ scripts/create_admin_user.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-output=flyctl ssh console --command "su - git -c '/usr/local/bin/gitea admin user list'" | grep "gitea"
+echo "checking for admin user"
+output=$(flyctl ssh console --command "su - git -c '/usr/local/bin/gitea admin user list'" | grep "gitea")
 if [ -z "$output" ]; then
-  password=flyctl ssh console --command "su - git -c '/usr/local/bin/gitea admin user create --username admin --password admin --email
+  password=$(flyctl ssh console --command "su - git -c '/usr/local/bin/gitea admin user create --username=gitea --random-password --email=gitea@brentgruber.com --admin --must-change-password=false'" | sed -n "/generated random password is/ s/.*'\(.*\)'.*/\1/p")
   doppler secrets set GITEA_ADMIN_PASSWORD=$password >/dev/null 2>&1
+  echo "Admin user gitea created"
 else
     echo "Admin user already exists"
 fi
