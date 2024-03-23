64861ba9613a2c99cab4c9f8b4ef79a624663dd3
BrentGruber <brent.gruber77@gmail.com>
2024-03-22 23:25:25 -0400

    fixing env variable name for actions runner

--- runner/scripts/start_runner.sh
+++ runner/scripts/start_runner.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo "starting runner"
-token=$(flyctl ssh console --command "docker run -e GITEA_INSTANCE_URL=https://gitea-brentgruberorg.fly.dev -e GITEA_RUNNER_TOKEN=$GITEA_ACTIONS_RUNNER_TOKEN -v /var/run/docker.sock:/var/run/docker.sock --restart always --name gitea_runner gitea/act_runner:latest")
+echo $GITEA_ACTIONS_RUNNER_TOKEN
+flyctl ssh console --command "docker run -d -e GITEA_INSTANCE_URL=https://gitea-brentgruberorg.fly.dev -e GITEA_RUNNER_REGISTRATION_TOKEN=$GITEA_ACTIONS_RUNNER_TOKEN -v /var/run/docker.sock:/var/run/docker.sock --restart always --name gitea_runner gitea/act_runner:latest"
