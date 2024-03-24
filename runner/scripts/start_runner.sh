609a95b4ee4f42f144d842c8a48877fb433b9dd2
BrentGruber <brent.gruber77@gmail.com>
2024-03-24 19:39:21 -0400

    run startup command on all machines

--- runner/scripts/start_runner.sh
+++ runner/scripts/start_runner.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 echo "starting runner"
-echo $GITEA_ACTIONS_RUNNER_TOKEN
-flyctl ssh console --command "docker run -d -e GITEA_INSTANCE_URL=https://gitea-brentgruberorg.fly.dev -e GITEA_RUNNER_REGISTRATION_TOKEN=$GITEA_ACTIONS_RUNNER_TOKEN -v /var/run/docker.sock:/var/run/docker.sock --restart always --name gitea_runner gitea/act_runner:latest"
+# for now, loop through all machines and run start command
+# would be nice if the docker start command could start the runner as well as dind
+for i in $(flyctl machines list -q); do
+    flyctl ssh console --command "docker run -d -e GITEA_INSTANCE_URL=https://gitea-brentgruberorg.fly.dev -e GITEA_RUNNER_REGISTRATION_TOKEN=$GITEA_ACTIONS_RUNNER_TOKEN -v /var/run/docker.sock:/var/run/docker.sock --restart always --name gitea_runner gitea/act_runner:latest"
+done
